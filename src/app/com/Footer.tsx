bbfef4453a2e946dbca893ee993e9a04f33e7ec2
w0010 <44535432+w0010@users.noreply.github.com>
2023-11-06 21:40:05 -0500

    polyspace added

---
+++ src/app/com/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-col items-center justify-between p-12">
+            <p>end</p>
+        </footer>
+    )
+}
---
+++ src/app/com/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex flex-col items-center justify-between p-12">
+          <h1>Brad Wooten</h1>
+        </header>
+    )
+}
